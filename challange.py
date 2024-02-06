0de4b10001f398f3671e12adce2b0ddb3c07ea30
GustavFlachs <152276245+GustavFlachs@users.noreply.github.com>
2024-02-06 12:26:22 +0100

    not done yet

---
+++ challange.py
@@ -0,0 +1,118 @@
+print("Vítejte v programu který převádí dekadickou IP adressu na binární ip adressu (prosím rozdělte na 4 části,nepoužívejte tečky prosím)")
+
+a = int(input("Zadejte první část dekadické IP adressi: "))
+b = int(input("Zadejte druhou část dekadické IP adressi: "))
+c = int(input("Zadejte třetí část dekadické IP adressi: "))
+d = int(input("Zadejte čtvrtou část dekadické IP adressi: : "))
+
+
+if a > 255:
+    print("nesmí být více jak 255")
+if a > 127:
+    print("1")
+    (a-127)
+if a > 63:
+    print("1")
+    (a-63)
+if a > 31:
+    print("1")
+    (a-31)
+if a > 15:
+    print("1")
+    (a-15)
+if a > 7:
+    print("1")
+    (a-7)
+if a > 3:
+    print("1")
+    (a-3)
+if a > 1:
+    print("1")
+    (a-1)
+if a > 0:
+    print("1")
+    (a-0)
+print(".")
+
+if b > 255:
+    print("nesmí být více jak 255")
+if b > 127:
+    print("1")
+    (b-127)
+if b > 63:
+    print("1")
+    (b-63)
+if b > 31:
+    print("1")
+    (b-31)
+if b > 15:
+    print("1")
+    (b-15)
+if b > 7:
+    print("1")
+    (b-7)
+if b > 3:
+    print("1")
+    b-3
+if b > 1:
+    print("1")
+    b-1
+if b > 0:
+    print("1")
+    b-0
+print(".")
+
+if c > 255:
+    print("nesmí být více jak 255")
+if c > 127:
+    print("1")
+    c-127
+if c > 63:
+    print("1")
+    c-63
+if c > 31:
+    print("1")
+    c-31
+if c > 15:
+    print("1")
+    c-15
+if c > 7:
+    print("1")
+    c-7
+if c > 3:
+    print("1")
+    c-3
+if c > 1:
+    print("1")
+    c-1
+if c > 0:
+    print("1")
+    c-0
+print(".")
+
+if d > 255:
+    print("nesmí být více jak 255")
+if d > 127:
+    print("1")
+    d-127
+if d > 63:
+    print("1")
+    d-63
+if d > 31:
+    print("1")
+    d-31
+if d > 15:
+    print("1")
+    d-15
+if d > 7:
+    print("1")
+    d-7
+if d > 3:
+    print("1")
+    d-3
+if d > 1:
+    print("1")
+    d-1
+if d > 0:
+    print("1")
+    d-0
